e13daa01e3339873f03325251647e8382aa3506e
Norman Cade <norman_cade32@yahoo.com>
2023-10-08 10:13:09 -0400

    change vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  rollupOptions: {
-    input: {
-      main: resolve(root, "index.html"),
-      success: resolve(root, "pages", "success", "index.html"),
-    },
-  },
 });
